44b8287f788efe8688c04acf34f38753ca72330e
TylerAi92 <twtiktokshop9@gmail.com>
2024-10-17 13:17:44 -0700

    Initial Commit

---
+++ Note Taker App/Note_Taker_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_Taker_AppApp.swift
+//  Note Taker App
+//
+//  Created by Tyler Weatherford on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Note_Taker_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Note Taker AppTests/Note_Taker_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Note_Taker_AppTests.swift
+//  Note Taker AppTests
+//
+//  Created by Tyler Weatherford on 10/17/24.
+//
+
+import Testing
+@testable import Note_Taker_App
+
+struct Note_Taker_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
